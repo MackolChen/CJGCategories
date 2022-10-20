2d413df862fe337b6f759ca4c6a8c812bf520e7a
chenjg <chenjg@tiens.com>
2022-10-20 14:34:26 +0800

    Initial commit

---
+++ Example/CJGCategories/CJGViewController.h
@@ -0,0 +1,13 @@
+//
+//  CJGViewController.h
+//  CJGCategories
+//
+//  Created by MackolChen on 10/20/2022.
+//  Copyright (c) 2022 MackolChen. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CJGViewController : UIViewController
+
+@end
---
+++ Example/CJGCategories/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CJGCategories
+//
+//  Created by MackolChen on 10/20/2022.
+//  Copyright (c) 2022 MackolChen. All rights reserved.
+//
+
+@import UIKit;
+#import "CJGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CJGAppDelegate class]));
+    }
+}
